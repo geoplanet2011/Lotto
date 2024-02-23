325a75edfa4c921ddbeca4fdec9dbe2b63a3647b
Beka <beka@gogichaishvili.ge>
2024-02-23 14:29:07 +0400

    add firebase recovery

--- app/src/main/java/ge/gogichaishvili/lotto/recovery/presentation/fragments/RecoveryFragment.kt
+++ app/src/main/java/ge/gogichaishvili/lotto/recovery/presentation/fragments/RecoveryFragment.kt
@@ -1,60 +1,76 @@
 package ge.gogichaishvili.lotto.recovery.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import ge.gogichaishvili.lotto.R
+import ge.gogichaishvili.lotto.databinding.FragmentRecoveryBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RecoveryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecoveryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentRecoveryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recovery, container, false)
+        _binding = FragmentRecoveryBinding.inflate(inflater, container, false)
+        return _binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecoveryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RecoveryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recoverBtn.setOnClickListener {
+            resetPassword()
+        }
+
+    }
+
+    private fun resetPassword() {
+        val email: String = binding.emailInput.text.trim().toString()
+        if (email.isEmpty()) {
+            Toast.makeText(requireContext(), R.string.enter_email, Toast.LENGTH_SHORT).show()
+        } else {
+            showLoader()
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    hideLoader()
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            requireContext(),
+                            R.string.reset_password,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            task.exception!!.message.toString(),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            }
+        }
+    }
+
+    private fun showLoader() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideLoader() {
+        if (binding.progressBar.isVisible) {
+            binding.progressBar.visibility = View.INVISIBLE
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
