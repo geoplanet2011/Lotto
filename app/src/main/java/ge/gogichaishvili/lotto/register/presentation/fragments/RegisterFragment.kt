da24ae7053c9396bcf0f0bb7e4795ca999a6a199
Beka <beka@gogichaishvili.ge>
2024-02-23 14:18:01 +0400

    add firebase user register

--- app/src/main/java/ge/gogichaishvili/lotto/register/presentation/fragments/RegisterFragment.kt
+++ app/src/main/java/ge/gogichaishvili/lotto/register/presentation/fragments/RegisterFragment.kt
@@ -1,60 +1,135 @@
 package ge.gogichaishvili.lotto.register.presentation.fragments
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import ge.gogichaishvili.lotto.R
+import ge.gogichaishvili.lotto.databinding.FragmentRegisterBinding
+import java.net.URI
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+    private var databaseReference: DatabaseReference? = null
+    private var database: FirebaseDatabase? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return _binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("Users")
+
+        binding.registerBtn.setOnClickListener {
+            register()
+        }
+
+        binding.pictureIV.setOnClickListener {
+
+        }
+
+        binding.cameraBtn.setOnClickListener {
+
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    private fun register() {
+
+        if (binding.emailInput.text.trim().toString().isEmpty() || binding.passwordInput.text.trim()
+                .toString().isEmpty()
+        ) {
+            Toast.makeText(requireContext(), R.string.input_required, Toast.LENGTH_SHORT).show()
+            return
+        }
+        createUser(
+            binding.emailInput.text.trim().toString(),
+            binding.passwordInput.text.trim().toString()
+        )
+    }
+
+    private fun createUser(email: String, password: String) {
+        showLoader()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) {
+                hideLoader()
+                if (it.isSuccessful) {
+                    val currentUser = auth.currentUser
+                    val currentUserDb = databaseReference?.child((currentUser?.uid!!))
+                    currentUserDb?.child("firstname")
+                        ?.setValue(binding.firstnameInput.text.trim().toString())
+                    Toast.makeText(
+                        requireContext(),
+                        R.string.registration_success,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(requireContext(), "Error", Toast.LENGTH_SHORT).show()
                 }
             }
     }
+
+    fun resizeBitmap(source: Bitmap, maxLength: Int): Bitmap {
+        try {
+            if (source.height >= source.width) {
+                if (source.height <= maxLength) {
+                    return source
+                }
+                val aspectRatio = source.width.toDouble() / source.height.toDouble()
+                val targetWidth = (maxLength * aspectRatio).toInt()
+                return Bitmap.createScaledBitmap(source, targetWidth, maxLength, false)
+            } else {
+                if (source.width <= maxLength) {
+                    return source
+                }
+                val aspectRatio = source.height.toDouble() / source.width.toDouble()
+                val targetHeight = (maxLength * aspectRatio).toInt()
+                return Bitmap.createScaledBitmap(source, maxLength, targetHeight, false)
+            }
+        } catch (e: Exception) {
+            return source
+        }
+    }
+
+    private fun showLoader() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideLoader() {
+        if (binding.progressBar.isVisible) {
+            binding.progressBar.visibility = View.INVISIBLE
+        }
+    }
+
 }
